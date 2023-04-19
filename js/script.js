cc17270416e91b7bed46b1e768f9284739291bef
Yuri-Gabutero24 <124618894+Yuri-Gabutero24@users.noreply.github.com>
2023-04-19 17:18:32 +0000

    webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Yuri G
+// Created on: April 2023
 // This file contains the JS functions for index.html
+
+
+'use strict'
+/**
+* This function is for calculating the volume of a pyramid
+*/
+function calculate() {
+// input
+  const radius = parseInt(document.getElementById('sphere-radius').value)
+
+
+ // process
+const volume = (4 / 3) * Math.PI * Math.pow(radius, 3)
+
+// output
+document.getElementById('volume').innerHTML = "The volume is " + volume + " cm³"
+}
